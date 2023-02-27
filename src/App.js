bf2db8a11546c145758a02c15cdef17b106068a7
Srbuhi <srbuhi.sarg@gmail.com>
2023-02-27 22:11:17 +0400

    Create ChatRoom component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { firestore, auth }   from './config.js';
 import { useAuthState }      from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { collection, orderBy, query, limit } from 'firebase/firestore';
 
 
 function App() {
@@ -38,6 +39,22 @@ function SignIn () {
 function SignOut () {
   return auth.currentUser && (
     <button onClick={ () => auth.signOut() }>Sign Out</button>
+  )
+}
+
+
+//Component ChatRoom
+function ChatRoom () {
+  const messageRef = collection(firestore , "messages");
+  const query = query(messageRef, orderBy("createdAt"), limit(25));
+  const [messages] = useCollectionData(query, {idField: 'id'})
+
+  return (
+    <>
+      <div>
+        {messages && messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
+    </>
   )
 }
 
