0ced6df3f6126d6f64def6d3c634830d58c71e63
Srbuhi <srbuhi.sarg@gmail.com>
2023-03-02 18:02:05 +0400

    Remove

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ function ChatMessage(props) {
   const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
   return (
     <div className={ `message ${messageClass}` }>
-      <img src={photoURL} alt="photo" />
+      <img src={photoURL}/>
       <p>{text}</p>
     </div>
   )
